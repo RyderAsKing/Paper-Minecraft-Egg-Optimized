7f12deb30e15e61be75c14dceeb21e9284e2a375
RyderAsKing <50543053+RyderAsKing@users.noreply.github.com>
2021-02-01 09:03:02 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function welcome() {
-    console.clear();
-    console.log('=================================================================================================================');
-    console.log('= Your Node JS Container has been setup. You may now upload your files using SFTP or the web based file manager =');
-    console.log('=================================================================================================================');
-}
-welcome();
-setInterval(welcome, 30000);
